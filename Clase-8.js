7c4cadcc80a0f4c9dfbdac62bbdde3ecb21a2da7
RandomBlueGuy <randombluemail@gmail.com>
2023-02-08 15:12:07 -0500

    Commit final

--- Clase-8.js
+++ Clase-8.js
@@ -7,10 +7,10 @@ console.log("/*----------------------------------------------------------*/");
 console.log(getVoteCount({upvotes:13, downvotes:0}));//RESULTADO ESPERADO:      13
 console.log(getVoteCount({upvotes:2, downvotes:33}));//RESULTADO ESPERADO:      -31
 console.log(getVoteCount({upvotes:132, downvotes:132}));//RESULTADO ESPERADO:   0
-console.log(getVoteCount({upvotes1:132, downvotes:132}))          
+console.log(getVoteCount({upvotes1:132, downvotes:132}))//RESULTADO ESPERADO:   "Una de las propiedades del objeto corresponde a .upvotes o .downvotes"  
 
 function getVoteCount(obj) {
-    return typeof(obj.upvotes) === 'number' && typeof(obj.downvotes) === 'number' ? obj.upvotes - obj.downvotes : "una de las propiedades del objeto corresponde a .upvotes o .downvotes";
+    return typeof(obj.upvotes) === 'number' && typeof(obj.downvotes) === 'number' ? obj.upvotes - obj.downvotes : "Una de las propiedades del objeto corresponde a .upvotes o .downvotes";
     //return obj.upvotes -git obj.downvotes;
 }
 
